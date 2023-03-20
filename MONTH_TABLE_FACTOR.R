5bad058baa09e1b457d83d1a3c111566d2d06370
indhu-v <113018637+indhu-v@users.noreply.github.com>
2023-03-20 23:33:30 +0530

    Add files via upload

---
+++ MONTH_TABLE_FACTOR.R
@@ -0,0 +1,10 @@
+ mons_v = c("March","April","January","November","January",
+             +            "September","October","September","November","August","February",
+             +            "January","November","November","February","May","August","February",
+             +            "July","December","August","August","September","November","September",
+             +            "February","April")
+ f=factor(mons_v)
+ f
+ table(f)
+ 
+ 
